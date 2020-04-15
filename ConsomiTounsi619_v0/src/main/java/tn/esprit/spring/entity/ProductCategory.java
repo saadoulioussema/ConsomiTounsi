516e278007b6bb5c68f44eba1ab047d9f2820554
MohamedAmineNjah <44654432+MohamedAmineNjah@users.noreply.github.com>
2020-04-15 01:34:43 +0100

    entities amine

--- ConsomiTounsi619_v0/src/main/java/tn/esprit/spring/entity/ProductCategory.java
+++ ConsomiTounsi619_v0/src/main/java/tn/esprit/spring/entity/ProductCategory.java
@@ -3,9 +3,13 @@ package tn.esprit.spring.entity;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 @Entity
@@ -14,67 +18,57 @@ public class ProductCategory implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = -1886298075027656464L;
-	@Id
+	@Id 
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
-	private Long Id;
+	private Long id ;
+	@Column(unique=true)
 	private String name;
 	@OneToMany(mappedBy="category")
+	@JsonIgnore
 	private List <Product> products;
+	
 	@OneToMany(mappedBy="productCategory")
 	private List <UserProductCategoryViews> productCategoryUsersViews;
-	public Long getId() {
-		return Id;
+	public Long getId () {
+		return id ;
 	}
-	public void setId(Long id) {
-		Id = id;
+	public void  setId (Long id ) {
+		this.id  = id ;
 	}
 	public String getName() {
 		return name;
 	}
-	public void setName(String name) {
+	public void  setName(String name) {
 		this.name = name;
 	}
 	public List<Product> getProducts() {
 		return products;
 	}
-	public void setProducts(List<Product> products) {
+	public void  setProducts(List<Product> products) {
 		this.products = products;
 	}
-	public ProductCategory(Long id, String name, List<Product> products) {
+	public ProductCategory(Long id , String name, List<Product> products) {
 		super();
-		Id = id;
+		this.id  =id ;
 		this.name = name;
 		this.products = products;
 	}
-	public List<UserProductCategoryViews> getProductCategoryUsersViews() {
-		return productCategoryUsersViews;
-	}
-	public void setProductCategoryUsersViews(List<UserProductCategoryViews> productCategoryUsersViews) {
-		this.productCategoryUsersViews = productCategoryUsersViews;
-	}
+	
 	public ProductCategory() {
 		super();
 	}
-	public ProductCategory(Long id, String name, List<Product> products,
-			List<UserProductCategoryViews> productCategoryUsersViews) {
-		super();
-		Id = id;
-		this.name = name;
-		this.products = products;
-		this.productCategoryUsersViews = productCategoryUsersViews;
-	}
+	
 	@Override
 	public String toString() {
-		return "ProductCategory [Id=" + Id + ", name=" + name + ", products=" + products
-				+ ", productCategoryUsersViews=" + productCategoryUsersViews + "]";
+		return "ProductCategory [id =" + id  + ", name=" + name + ", products=" + products
+				+ "]";
 	}
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		result = prime * result + ((id  == null) ? 0 : id .hashCode());
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((productCategoryUsersViews == null) ? 0 : productCategoryUsersViews.hashCode());
 		result = prime * result + ((products == null) ? 0 : products.hashCode());
 		return result;
 	}
@@ -87,21 +81,17 @@ public class ProductCategory implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		ProductCategory other = (ProductCategory) obj;
-		if (Id == null) {
-			if (other.Id != null)
+		if (id  == null) {
+			if (other.id  != null)
 				return false;
-		} else if (!Id.equals(other.Id))
+		} else if (!id .equals(other.id ))
 			return false;
 		if (name == null) {
 			if (other.name != null)
 				return false;
 		} else if (!name.equals(other.name))
 			return false;
-		if (productCategoryUsersViews == null) {
-			if (other.productCategoryUsersViews != null)
-				return false;
-		} else if (!productCategoryUsersViews.equals(other.productCategoryUsersViews))
-			return false;
+
 		if (products == null) {
 			if (other.products != null)
 				return false;
@@ -109,6 +99,7 @@ public class ProductCategory implements Serializable{
 			return false;
 		return true;
 	}
+	
 	
 	
 }
