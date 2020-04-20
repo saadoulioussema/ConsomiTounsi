5a6f8b1a192ad49d1baf4b93aa067db3804d7bc7
MohamedAmineNjah <44654432+MohamedAmineNjah@users.noreply.github.com>
2020-04-20 02:25:44 +0100

    video1

--- ConsomiTounsi619_v0/src/main/java/tn/esprit/spring/controller/RestRayController.java
+++ ConsomiTounsi619_v0/src/main/java/tn/esprit/spring/controller/RestRayController.java
@@ -2,6 +2,9 @@
 package tn.esprit.spring.controller;
 
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -18,8 +21,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import tn.esprit.spring.entity.Category;
 import tn.esprit.spring.entity.Notif;
@@ -312,6 +317,20 @@ public class RestRayController {
 			    	
 			    	return new ResponseEntity<>(HttpStatus.CREATED);
 			    }
+				
+				
+				@RequestMapping(value="/video", method=RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+				public ResponseEntity<Object> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+					File convertFile = new File("C:\\Users\\Lenovo\\Desktop\\zz\\"+file.getOriginalFilename());
+					convertFile.createNewFile();
+					FileOutputStream fout = new FileOutputStream(convertFile);
+					
+					
+					
+					fout.write(file.getBytes());
+					fout.close();
+					return new ResponseEntity<>("File is uploaded successfully", HttpStatus.OK);
+				}
 				
 
 }
