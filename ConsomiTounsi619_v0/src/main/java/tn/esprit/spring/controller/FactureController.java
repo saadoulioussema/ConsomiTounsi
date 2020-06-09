0300ce3138496c860cf86422f0c0a63db65a7eed
LENOVO <LENOVO@192.168.1.9>
2020-06-09 13:13:33 +0200

    updating template and some jsf pages by Alaeddine

--- ConsomiTounsi619_v0/src/main/java/tn/esprit/spring/controller/FactureController.java
+++ ConsomiTounsi619_v0/src/main/java/tn/esprit/spring/controller/FactureController.java
@@ -1,34 +1,15 @@
 package tn.esprit.spring.controller;
 
-import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.itextpdf.text.pdf.codec.Base64.InputStream;
-
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
-import java.util.Scanner;
-
 import javax.servlet.ServletContext;
-
 import tn.esprit.spring.GeneratePdfReport;
 import tn.esprit.spring.entity.Facture;
 import tn.esprit.spring.entity.Panier;
